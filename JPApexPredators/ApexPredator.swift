b45ce364e40c3d1764519ca1a275173dbfbf0a62
Ibrahim-Lokman <ibrahimlokman.bd@gmail.com>
2024-09-13 11:24:16 +0600

    Parent and child alignment ForEach id Link

--- JPApexPredators/ApexPredator.swift
+++ JPApexPredators/ApexPredator.swift
@@ -24,7 +24,7 @@ struct ApexPredator: Codable, Identifiable {
             .replacingOccurrences(of: " ", with: "")
     }
     
-    struct MovieScene: Codable {
+    struct MovieScene: Codable, Identifiable {
         let id: Int
         let movie: String
         let sceneDescription: String
--- JPApexPredators/PredatorDetail.swift
+++ JPApexPredators/PredatorDetail.swift
@@ -25,6 +25,44 @@ struct PredatorDetail: View {
                         .shadow(color: /*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/, radius: 7)
                         .offset(y: 20)
                 }
+                VStack (alignment: .leading) {
+                    Text(predator.name)
+                        .font(.largeTitle)
+                    Text("Appears In:")
+                        .font(.title3)
+                    ForEach(predator.movies, id: \.self) { movie in
+                        Text("◎ "+movie)
+                            .font(.subheadline)
+                    }
+                    Text("Movie Moments")
+                        .font(.title)
+                        .padding(.top, 15)
+                    ForEach(predator.movieScenes) { scene in
+                        
+                        Text(scene.movie)
+                            .font(.title2)
+                            .padding(.vertical, 1)
+                        
+                        Text(scene.sceneDescription)
+                            .padding(.bottom, 15)
+                        
+                      
+                  
+                    }
+                    Text("Read More:")
+                        .font(.caption)
+                    
+                    Link(predator.link, destination:
+                            URL(string: predator.link)!
+                        )
+                        .font(.caption)
+                        .foregroundStyle(.blue)
+                    
+                   
+                }
+                .padding()
+                .padding(.bottom)
+                .frame(width: geo.size.width, alignment: .leading)
             }.ignoresSafeArea()
         }
        
